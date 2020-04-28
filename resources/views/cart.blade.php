253a8e911ff4b5bcbb28c4f0856db0f4ab715b6f
BunnyShinny <shinewanna27199@gmail.com>
2020-04-28 19:22:01 +0630

    cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -106,7 +106,7 @@
                     <form action="" class="cart_coupon_form d-flex flex-row align-items-start justify-content-start"
                         id="cart_coupon_form">
                         
-                        <a href="booktable"><button class="button_clear cart_button_2">RESERVE</button></a>
+                        <a href="bookdisplay" class="button_clear cart_button_2">RESERVE</a>
                     </form>
                 </div>
             </div>
